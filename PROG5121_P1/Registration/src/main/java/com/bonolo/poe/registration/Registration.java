1f3d14104052749b688e77e4fc769966aefbf04a
bonolotomeng390-lab <bonolotomeng390@gmail.com>
2026-09-18 10:45:57 +0200

    Fix: track PROG5121_P1 as regular files instead of submodule

---
+++ PROG5121_P1/Registration/src/main/java/com/bonolo/poe/registration/Registration.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.bonolo.poe.registration;
+
+/**
+ *
+ * @author bonol
+ */
+public class Registration {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
